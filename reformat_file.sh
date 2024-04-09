efb11e551f6ab2f5ddc26e1010bf80babb293b8a
Lars Schimmelpfennig <lschimmelpfe@wisc.edu>
2024-04-09 12:23:26 -0500

    Working reformat_file.sh

--- reformat_file.sh
+++ reformat_file.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+file_name=$1
+
+cat $file_name | tr '[:upper:]' '[:lower:]' | sed -e 's/[[:punct:]]//g' | sed -e 's/\t\t*/\n/g' | sed -e 's/  */\n/g' | grep -v -e '^$' | sort > sorted_file_$file_name
+
+#sort sorted_file_$file_name > sorted_file_$file_name
